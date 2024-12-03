ab2c96fafae581e5faff00d878f534921e850300
Eipek95 <eipek1995@gmail.com>
2024-12-03 16:33:47 +0300

    edit

--- SharedLibrary/DTOs/NoDataDto.cs
+++ SharedLibrary/DTOs/NoDataDto.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SharedLibrary.DTOs
+﻿namespace SharedLibrary.DTOs
 {
-    internal class NoDataDto
+    public class NoDataDto
     {
     }
 }
